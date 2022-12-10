13fb1f522f2cc386fee0ffee4f746d850c443b6e
HadarNeu <hadarnoylander@gmail.com>
2022-12-10 12:30:53 +0200

    deleting irrelevant files

--- test.py
+++
@@ -1,8 +0,0 @@
-from subprocess import check_output
-
-def local_ipv4():
-    internalIP = check_output(['hostname', '-I'])
-    internalIP = str(internalIP)[2:-3]
-    return internalIP
-
-print(local_ipv4(), type(local_ipv4()))
